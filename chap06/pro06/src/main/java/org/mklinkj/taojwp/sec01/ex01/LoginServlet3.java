6cc04559e03ba9af6337d97f7142dad36c26750e
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-02-25 06:53:54 +0900

    6.5.3 POST 방식으로 서블릿에 요청

---
+++ chap06/pro06/src/main/java/org/mklinkj/taojwp/sec01/ex01/LoginServlet3.java
@@ -0,0 +1,38 @@
+package org.mklinkj.taojwp.sec01.ex01;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebServlet("/login3")
+public class LoginServlet3 extends HttpServlet {
+  private static final String UTF_8_ENCODING = StandardCharsets.UTF_8.name();
+
+  @Override
+  public void init() {
+    LOGGER.info("init 메서드 호출");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    request.setCharacterEncoding(UTF_8_ENCODING);
+    response.setContentType(String.format("text/html;charset=%s", UTF_8_ENCODING));
+
+    String userId = request.getParameter("user_id");
+    String password = request.getParameter("user_pw");
+
+    LOGGER.info("아이디: {}", userId);
+    LOGGER.info("비밀번호: {}", password);
+  }
+
+  @Override
+  public void destroy() {
+    LOGGER.info("destroy 메서드 호출");
+  }
+}
---
+++ chap06/pro06/src/test/java/org/mklinkj/taojwp/sec01/ex01/LoginServlet3Test.java
@@ -0,0 +1,28 @@
+package org.mklinkj.taojwp.sec01.ex01;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mklinkj.taojwp.test.support.MockHttpServletTestSupport;
+import org.springframework.http.HttpStatus;
+
+class LoginServlet3Test extends MockHttpServletTestSupport<LoginServlet3> {
+
+  @BeforeEach
+  void beforeEach() {
+    request.setServletPath("/login3");
+    setServlet(new LoginServlet3());
+  }
+
+  @Test
+  void testDoPost() throws IOException {
+    request.setParameter("user_id", "park");
+    request.setParameter("user_pw", "1234");
+
+    servlet.doPost(request, response);
+
+    assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+  }
+}
