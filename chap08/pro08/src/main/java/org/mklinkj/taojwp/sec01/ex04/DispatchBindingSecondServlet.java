b626e8d9b55fc5aba5ec3c8622988eddb11429cf
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-03-01 07:25:57 +0900

    미사용 Checked 예외를 괜히 추가해서 테스트가 실패한다.

--- chap08/pro08/src/main/java/org/mklinkj/taojwp/sec01/ex04/DispatchBindingSecondServlet.java
+++ chap08/pro08/src/main/java/org/mklinkj/taojwp/sec01/ex04/DispatchBindingSecondServlet.java
@@ -3,7 +3,6 @@ package org.mklinkj.taojwp.sec01.ex04;
 import static org.mklinkj.taojwp.common.Constants.HTML_CONTENT_TYPE;
 import static org.mklinkj.taojwp.common.Constants.SERVER_ENCODING;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,7 +16,7 @@ import org.apache.commons.text.StringSubstitutor;
 public class DispatchBindingSecondServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException, ServletException {
+      throws IOException {
     request.setCharacterEncoding(SERVER_ENCODING);
     response.setContentType(HTML_CONTENT_TYPE);
     PrintWriter out = response.getWriter();
