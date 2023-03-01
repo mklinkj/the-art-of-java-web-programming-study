d8007cfbba0c6a92d770660747498e8a6bca56f9
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-03-02 03:52:36 +0900

    9.3.4 세션 쿠키 사용하기 - 완료
    - 테스트 코드에서 수명을 구분을 구분하는 부분은 없어서, 테스트 코드 수정사항은 없다.

--- chap09/pro09/src/main/java/org/mklinkj/taojwp/sec02/ex01/SetCookieValue.java
+++ chap09/pro09/src/main/java/org/mklinkj/taojwp/sec02/ex01/SetCookieValue.java
@@ -27,7 +27,8 @@ public class SetCookieValue extends HttpServlet {
 
     Cookie c = new Cookie("cookieTest", URLEncoder.encode("JSP프로그래밍입니다.", SERVER_ENCODING));
 
-    c.setMaxAge(24 * 60 * 60);
+    // c.setMaxAge(24 * 60 * 60);
+    c.setMaxAge(-1);
 
     response.addCookie(c);
 
