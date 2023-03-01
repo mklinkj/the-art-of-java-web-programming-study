ca02cdcacdf9eee9d503ddd2be90b979ba922137
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-03-01 22:17:49 +0900

    9.2.1 `<hidden>` 태그를 이용한 세션 트래킹 실습 - 완료
    9장 부터는 테스트 코드에 mockito 라이브러리를 적용해보자!

---
+++ chap09/pro09/src/main/java/org/mklinkj/taojwp/sec01/ex01/LoginServlet.java
@@ -0,0 +1,70 @@
+package org.mklinkj.taojwp.sec01.ex01;
+
+import static org.mklinkj.taojwp.common.Constants.HTML_CONTENT_TYPE;
+import static org.mklinkj.taojwp.common.Constants.SERVER_ENCODING;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.text.StringSubstitutor;
+
+@Slf4j
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+  @Override
+  public void init() {
+    LOGGER.info("init 메서드 호출");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    request.setCharacterEncoding(SERVER_ENCODING);
+    response.setContentType(HTML_CONTENT_TYPE);
+    PrintWriter out = response.getWriter();
+
+    String userId = request.getParameter("user_id");
+    String userPw = request.getParameter("user_pw");
+    String userAddress = request.getParameter("user_address");
+    String userEmail = request.getParameter("user_email");
+    String userHp = request.getParameter("user_hp");
+
+    out.print(
+        StringSubstitutor.replace(
+            """
+            <!DOCTYPE html>
+            <html lang="ko">
+            <head>
+              <meta charset="UTF-8">
+              <title>로그인 정보</title>
+            </head>
+            <body>
+              <h4>안녕하세요!<br>로그인하셨습니다.</h4>
+              아이디: ${userId}<br>
+              비밀번호: ${userPw}<br>
+              주소: ${userAddress}<br>
+              email: ${userEmail}<br>
+              휴대 전화: ${userHp}<br>
+            </body>
+            </html>
+            """,
+            Map.of(
+                "userId",
+                userId,
+                "userPw",
+                userPw,
+                "userAddress",
+                userAddress,
+                "userEmail",
+                userEmail,
+                "userHp",
+                userHp)));
+  }
+}
--- chap09/pro09/src/test/java/org/mklinkj/taojwp/test/support/MockHttpServletTestSupport.java
+++ chap09/pro09/src/test/java/org/mklinkj/taojwp/test/support/MockHttpServletTestSupport.java
@@ -1,7 +1,10 @@
 package org.mklinkj.taojwp.test.support;
 
+import static org.mklinkj.taojwp.test.support.TestConstants.CONTEXT_PATH;
+
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServlet;
+import org.mockito.Mockito;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockServletConfig;
@@ -9,13 +12,10 @@ import org.springframework.mock.web.MockServletContext;
 
 /** Mock HTTP Servlet 테스트를 위한 공통 테스트 지원 클래스 */
 public abstract class MockHttpServletTestSupport<T extends HttpServlet> {
-
   protected T servlet;
   protected MockHttpServletRequest request;
-
   protected MockHttpServletResponse response;
   protected MockServletContext servletContext;
-
   protected MockServletConfig servletConfig;
 
   public MockHttpServletTestSupport() {
@@ -26,8 +26,10 @@ public abstract class MockHttpServletTestSupport<T extends HttpServlet> {
     servletContext = createMockServletContext();
     servletConfig = createMockServletConfig(servletContext);
     request = new MockHttpServletRequest(servletContext);
+    request.setContextPath(getContextPath());
+    request.setServletPath(getServletPath());
     response = new MockHttpServletResponse();
-    request.setContextPath("/pro08");
+    servlet = mockServlet();
   }
 
   private MockServletConfig createMockServletConfig(ServletContext context) {
@@ -38,7 +40,19 @@ public abstract class MockHttpServletTestSupport<T extends HttpServlet> {
     return new MockServletContext();
   }
 
-  protected void setServlet(T servlet) {
-    this.servlet = servlet;
+  private T mockServlet() {
+    Class<T> servletClass = getServletClass();
+    T mockServlet = Mockito.mock(servletClass, Mockito.CALLS_REAL_METHODS);
+    Mockito.when(mockServlet.getServletConfig()).thenReturn(servletConfig);
+    Mockito.when(mockServlet.getServletContext()).thenReturn(servletContext);
+    return mockServlet;
   }
+
+  protected abstract Class<T> getServletClass();
+
+  protected String getContextPath() {
+    return CONTEXT_PATH;
+  }
+
+  protected abstract String getServletPath();
 }
