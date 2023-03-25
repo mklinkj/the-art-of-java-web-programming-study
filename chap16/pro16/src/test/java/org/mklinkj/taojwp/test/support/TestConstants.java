01c6ff26c34996d5a84c74e48a62eba61e73c218
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-03-25 15:50:56 +0900

    project data 유틸리티 기능 추가

--- chap16/pro16/src/test/java/org/mklinkj/taojwp/test/support/TestConstants.java
+++ chap16/pro16/src/test/java/org/mklinkj/taojwp/test/support/TestConstants.java
@@ -2,8 +2,9 @@ package org.mklinkj.taojwp.test.support;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.mklinkj.taojwp.common.util.ProjectDataUtils;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TestConstants {
-  public static final String CONTEXT_PATH = "/pro16";
+  public static final String CONTEXT_PATH = "/" + ProjectDataUtils.getProperty("project.name");
 }
