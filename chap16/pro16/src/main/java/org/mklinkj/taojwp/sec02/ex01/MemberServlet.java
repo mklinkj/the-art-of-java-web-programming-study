a010c12463f04a78baa6637048799d5f38947ff4
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-03-26 01:10:05 +0900

    JNDI로 가져오는 데이터소스가 여러번 호출해도 동일한지 확인.
    ==> 동일하다.

--- chap16/pro16/src/main/java/org/mklinkj/taojwp/sec02/ex01/MemberServlet.java
+++ chap16/pro16/src/main/java/org/mklinkj/taojwp/sec02/ex01/MemberServlet.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.mklinkj.taojwp.common.servlet.AbstractHttpServlet;
+import org.mklinkj.taojwp.common.util.DBUtils;
 
 @Slf4j
 @WebServlet("/mem")
@@ -19,6 +20,11 @@ public class MemberServlet extends AbstractHttpServlet {
       throws IOException {
     request.setCharacterEncoding(UTF_8_ENCODING);
     response.setContentType(HTML_CONTENT_TYPE);
+
+    // 서블릿의 기능과는 관계없지만... 계속 데이터소스를 계속 불러와서 동일한 데이터소스를 반환하는지 궁금해서...
+    LOGGER.info(
+        "## DataSource가 동일함 {} ##",
+        DBUtils.getDataSourceFromJNDI() == DBUtils.getDataSourceFromJNDI());
 
     PrintWriter writer = response.getWriter();
     String id = request.getParameter("id");
