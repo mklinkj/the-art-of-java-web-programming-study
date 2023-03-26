8730f2c6f81e7297e0911576b86cd082252e8105
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-03-27 00:38:56 +0900

    MyBatis에서 트랜젝션 사용 확인 - 같은 세션 레벨
    * https://github.com/mklinkj/the-art-of-java-web-programming-study/issues/9

--- chap16/pro16/src/test/java/org/mklinkj/taojwp/sec02/ex01/MemberDAOTest.java
+++ chap16/pro16/src/test/java/org/mklinkj/taojwp/sec02/ex01/MemberDAOTest.java
@@ -1,14 +1,17 @@
 package org.mklinkj.taojwp.sec02.ex01;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mklinkj.taojwp.common.util.SqlSessionFactoryHelper.sqlSessionFactory;
 import static org.mklinkj.taojwp.common.util.TransactionFactoryHelper.newTransaction;
 
 import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.transaction.Transaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mklinkj.taojwp.common.util.DBUtils;
+import org.mklinkj.taojwp.mapper.MemberMapper;
 
 @Slf4j
 class MemberDAOTest {
@@ -29,9 +32,8 @@ class MemberDAOTest {
 
   /** */
   @Test
-  void testInsertMember() throws Exception {
+  void testInsertMembers00() throws Exception {
     final Transaction tx = newTransaction();
-
     try {
       MemberVO vo0 = new MemberVO();
       vo0.setId("test00");
@@ -54,6 +56,35 @@ class MemberDAOTest {
       LOGGER.info("### tx.commit() 실행 후... ###");
     } finally {
       tx.close(); // 트랜젝션을 close()하면 Connection을 close하고 autoCommit 상태를 true로 바꾼다.
+    }
+
+    assertThat(dao.overlappedId("test00")).isTrue();
+    assertThat(dao.overlappedId("test01")).isTrue();
+    assertThat(dao.overlappedId("noUser00")).isFalse();
+    assertThat(dao.overlappedId("noUser01")).isFalse();
+  }
+
+  @Test
+  void testInsertMembers01() {
+    try (SqlSession sqlSession = sqlSessionFactory().openSession()) {
+      MemberVO vo0 = new MemberVO();
+      vo0.setId("test00");
+      vo0.setPwd("1234");
+      vo0.setName("테스트유저00");
+      vo0.setEmail("test00@test.com");
+      vo0.setJoinDate(LocalDateTime.of(2023, 12, 25, 0, 0));
+
+      MemberVO vo1 = new MemberVO();
+      vo1.setId("test01");
+      vo1.setPwd("1234");
+      vo1.setName("테스트유저01");
+      vo1.setEmail("test01@test.com");
+      vo1.setJoinDate(LocalDateTime.of(2023, 12, 25, 1, 0));
+
+      sqlSession.insert(MemberMapper.class.getCanonicalName().concat(".insertMember"), vo0);
+      sqlSession.insert(MemberMapper.class.getCanonicalName().concat(".insertMember"), vo1);
+
+      sqlSession.commit();
     }
 
     assertThat(dao.overlappedId("test00")).isTrue();
