521240ce6473690f747a319a8335763b278e1a37
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-07-01 00:37:59 +0900

    MyBatis 설정을 application.yml 에서 정의
    mybatis-spring-boot-starter가 여러 설정들을 application.yml에서 정의할 수 있게 해주기 때문에 특별히 설정 클래스를 만들필요는 없었음..
    
    더 복잡한 설정이 필요하면 어떻게 될진 모르겠지만...
    
    - https://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/index.html

--- chap30/my-board-boot2/src/main/java/org/mklinkj/taojwp/MyBoardBoot2Application.java
+++ chap30/my-board-boot2/src/main/java/org/mklinkj/taojwp/MyBoardBoot2Application.java
@@ -2,8 +2,6 @@ package org.mklinkj.taojwp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
 @SpringBootApplication
 public class MyBoardBoot2Application {
