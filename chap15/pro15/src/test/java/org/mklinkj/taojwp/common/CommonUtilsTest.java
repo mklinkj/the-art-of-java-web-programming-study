d645d0b263ab33f9e75ce4cb05b5a276c6f2ddae
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-11-22 20:04:07 +0900

    Jenkins 빌드/테스트시 윈도우 경로를 사용하는 코드가 실패하는 문제
    #15

--- chap15/pro15/src/test/java/org/mklinkj/taojwp/common/CommonUtilsTest.java
+++ chap15/pro15/src/test/java/org/mklinkj/taojwp/common/CommonUtilsTest.java
@@ -3,6 +3,8 @@ package org.mklinkj.taojwp.common;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 class CommonUtilsTest {
   @Test
@@ -13,6 +15,7 @@ class CommonUtilsTest {
   }
 
   @Test
+  @EnabledOnOs(OS.WINDOWS)
   void testFileNameOnly() {
     assertThat(CommonUtils.fileNameOnly("C:\\Test\\a.txt")).isEqualTo("a.txt");
     assertThat(CommonUtils.fileNameOnly("C:/Test/b.txt")).isEqualTo("b.txt");
