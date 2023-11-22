9c3079f9c1c37d626e225644e53ef77d19330a5e
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-11-22 20:34:49 +0900

    Jenkins 빌드/테스트시 윈도우 경로를 사용하는 코드가 실패하는 문제
    * #15
    * 관련 코드를 좀 더 수정했다.

--- chap15/pro15-servlet4/src/test/java/org/mklinkj/taojwp/common/CommonUtilsTest.java
+++ chap15/pro15-servlet4/src/test/java/org/mklinkj/taojwp/common/CommonUtilsTest.java
@@ -3,6 +3,9 @@ package org.mklinkj.taojwp.common;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 class CommonUtilsTest {
   @Test
@@ -13,9 +16,18 @@ class CommonUtilsTest {
   }
 
   @Test
+  @EnabledOnOs(OS.WINDOWS)
   void testFileNameOnly() {
     assertThat(CommonUtils.fileNameOnly("C:\\Test\\a.txt")).isEqualTo("a.txt");
     assertThat(CommonUtils.fileNameOnly("C:/Test/b.txt")).isEqualTo("b.txt");
+    assertThat(CommonUtils.fileNameOnly("c.txt")).isEqualTo("c.txt");
+  }
+
+  @Test
+  @DisabledOnOs(OS.WINDOWS)
+  void testFileNameOnly_Unix() {
+    assertThat(CommonUtils.fileNameOnly("/Test/a.txt")).isEqualTo("a.txt");
+    assertThat(CommonUtils.fileNameOnly("/Test/b.txt")).isEqualTo("b.txt");
     assertThat(CommonUtils.fileNameOnly("c.txt")).isEqualTo("c.txt");
   }
 }
--- chap15/pro15/src/test/java/org/mklinkj/taojwp/common/CommonUtilsTest.java
+++ chap15/pro15/src/test/java/org/mklinkj/taojwp/common/CommonUtilsTest.java
@@ -3,6 +3,7 @@ package org.mklinkj.taojwp.common;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
@@ -19,6 +20,14 @@ class CommonUtilsTest {
   void testFileNameOnly() {
     assertThat(CommonUtils.fileNameOnly("C:\\Test\\a.txt")).isEqualTo("a.txt");
     assertThat(CommonUtils.fileNameOnly("C:/Test/b.txt")).isEqualTo("b.txt");
+    assertThat(CommonUtils.fileNameOnly("c.txt")).isEqualTo("c.txt");
+  }
+
+  @Test
+  @DisabledOnOs(OS.WINDOWS)
+  void testFileNameOnly_Unix() {
+    assertThat(CommonUtils.fileNameOnly("/Test/a.txt")).isEqualTo("a.txt");
+    assertThat(CommonUtils.fileNameOnly("/Test/b.txt")).isEqualTo("b.txt");
     assertThat(CommonUtils.fileNameOnly("c.txt")).isEqualTo("c.txt");
   }
 }
